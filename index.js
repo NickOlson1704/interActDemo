e5a297c538efbd3761fc87260d8ff05912edac95
Nicholas Olson <naolson1704@gmail.com>
2023-04-26 16:37:01 -0500

    interActDemo

---
+++ index.js
@@ -0,0 +1,43 @@
+const minusButton = document.getElementById('minus-btn')
+const resetButton = document.getElementById('reset-btn')
+const plusButton = document.getElementById('plus-btn')
+const counterElement = document.getElementById('counter')
+const buttonList = document.querySelectorAll('.theme-buttons')
+
+let count = 0
+
+plusButton.addEventListener('click', () => {
+    count++
+    counterElement.innerHTML = count
+})
+minusButton.addEventListener('click', () => {
+    count--
+    counterElement.innerHTML = count
+})
+resetButton.addEventListener('click', () => {
+    count = 0
+    counterElement.innerHTML = count
+})
+
+const themeButtonClickHandler = (event) => {
+    let themeButton = event.target
+    let theme = themeButton.innerHTML
+
+    let body = document.querySelector('body')
+    let main = document.querySelector('main')
+    let allButtons = document.querySelectorAll('button')
+
+    body.classList = ""
+    body.classList.add(theme)
+    main.classList = ""
+    main.classList.add(theme)
+    for ( let i = 0; i < allButtons.length; i++) {
+        allButtons[i].classList = ""
+        allButtons[i].classList.add(theme)
+    }
+    
+}
+
+for (let  i = 0; i < buttonList.length; i++) {
+    buttonList[i].addEventListener('click', themeButtonClickHandler)
+}
